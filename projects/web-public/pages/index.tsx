83be1e24748ee2832106d9122661ee9d89dc3d7f
pvalcamartins <victorrjtijuca@gmail.com>
2022-08-04 11:57:50 -0300

    comitando alterações no web-public

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -4,7 +4,7 @@ import { sum } from '@alura/utils/math/sum';
 export default function HomeScreen() {
     return (
         <div>
-            <Text tag="h1">Home do Projeto</Text>
+            <Text tag="h1">Home do Projeto Site</Text>
             <Text tag="p">@alura/utils/math/sum: sum(2,2) = {sum(2,2)}</Text>
         </div>
     );
